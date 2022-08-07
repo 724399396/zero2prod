ad5b2c082a689a63cca7f4d72710776ddb71e836
Wei Li <liwei72439936@gmail.com>
2022-08-07 20:55:00 +0800

    update

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -1,4 +1,7 @@
+use wiremock::{Mock, ResponseTemplate};
+
 use crate::helpers::spawn_app;
+use wiremock::matchers::{method, path};
 
 #[actix_rt::test]
 async fn health_check_works() {
@@ -21,17 +24,32 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursual_le_guin%40gmail.com";
 
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
     let response = app.post_subscriptions(body.into()).await;
 
     assert_eq!(200, response.status().as_u16());
+}
 
-    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+#[actix_rt::test]
+async fn subscribe_persists_the_new_subscriber() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursual_le_guin%40gmail.com";
+
+    app.post_subscriptions(body.into()).await;
+
+    let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
 
     assert_eq!(saved.email, "ursual_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.status, "pending_confirmation");
 }
 
 #[actix_rt::test]
@@ -73,4 +91,38 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
             description
         )
     }
+}
+
+#[actix_rt::test]
+async fn susbscribe_sends_a_confirmation_email_for_valid_data() {
+    let app = spawn_app().await;
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+}
+
+#[actix_rt::test]
+async fn subscribe_sends_a_confirmation_email_with_a_link() {
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursual_le_guin%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let confirmation_links = app.get_confirmation_links(email_request);
+
+    assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
